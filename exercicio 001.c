1140d7fcb2e5ff824de22292e45d35af9e19f2a8
Sandra Valcacer <120135756+belavalcacer@users.noreply.github.com>
2022-12-15 12:05:08 -0400

    Add files via upload

---
+++ exercicio 001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int num1, num2, soma;
+  printf("Digite o primeiro numero: "); scanf("%d", &num1);
+  printf("Digite o segundo numero: "); scanf("%d", &num2);
+  soma = num1+num2;
+  printf("Resultado da soma: %.2d", soma);
+  return 0;
+}
