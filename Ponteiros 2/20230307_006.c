d4f3015ff080dd23f89714650043032fd8139c20
Sandra Valcacer <120135756+belavalcacer@users.noreply.github.com>
2023-03-28 14:07:14 -0300

    Update 20230307_006.c

--- Ponteiros 2/20230307_006.c
+++ Ponteiros 2/20230307_006.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int soma(int *v1, int *v2, int *v3, int tama, int tamb) {
+    if (tama != tamb) {
+        return 0;
+    }
 
-int main () {
-    int inteiro = 10;
-    float real = 4.1;
-    char caractere = 'e';
+    for (int i = 0; i < tama; i++) {
+        *(v3 + i) = *(v1 + i) + *(v2 + i);
+    }
 
-    int *pnteiro = &inteiro;
-    float *preal = &real;
-    char *pcarac = &caractere;
+    return 1;
+}
 
-    printf("Valores antes da alteracao: %d\n", inteiro);
-    printf("Valores antes da alteracao: %.2f\n", real);
-    printf("Valores antes da alteracao: %c\n", caractere);
+int main() {
+    int v1[] = {4,6,9,3,8,5};
+    int v2[] = {2,6,4,8,5,7};
+    int v3[6];
 
-    *pnteiro = 7;
-    *preal = 10.0;
-    *pcarac = 'i';
+    int x = soma(v1, v2, v3, 6, 6);
 
-    printf("Valores depois da alteracao: %d\n", inteiro);
-    printf("Valores depois da alteracao: %.2f\n", real);
-    printf("Valores depois da alteracao: %c\n", caractere);
-
-
-
-
+    if (x) {
+        printf("Soma dos vetores: %d %d %d %d %d %d\n", v3[0], v3[1], v3[2], v3[3], v3[4], v3[5]);
+    }
+    else {
+        printf("Vetores de tamanhos diferentes\n");
+    }
 
     return 0;
 }
-
